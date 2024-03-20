4274dcf7434d62cc402cc7063e272d1328047dec
Audun Øygard <audun@vev.design>
2024-03-20 15:55:58 +0100

    Add unsplash asset source

---
+++ unsplash/src/unsplash.ts
@@ -0,0 +1,98 @@
+import { registerVevPlugin } from "@vev/react";
+import { EditorPluginKv, EditorPluginType } from "@vev/utils";
+import { ProjectAsset, ProjectImageAsset } from "./vev-types";
+import { getPath, getPropertiesFromRequest } from "./settings";
+
+const API = "https://api.unsplash.com/";
+
+interface Photo {
+  id: string;
+  urls: { full: string; small: string };
+  user: { first_name: string; last_name: string };
+  links: { html: string };
+  width: number;
+  height: number;
+  description: string;
+}
+
+function mapAssetToVevAsset(photos: Photo[] = []): ProjectAsset[] {
+  return photos.map((photo) => {
+    return {
+      key: photo.id,
+      url: photo.urls.full,
+      filename: `Unsplash | ${photo.user.first_name} ${
+        photo.user.last_name || ""
+      }`,
+      mimeType: "image/jpg",
+      dimension: { width: photo.width, height: photo.height },
+      selfHosted: false,
+      thumb: photo.urls.small,
+      metaData: {
+        description: photo.description,
+        width: `${photo.width}`,
+        height: `${photo.height}`,
+        user: `${photo.user.first_name} ${photo.user.last_name}`,
+        link: photo.links.html,
+      },
+    } as ProjectImageAsset;
+  });
+}
+
+export type MetaFields = Record<string, string[]>;
+
+async function handler(
+  request: Request,
+  env: Record<string, string>,
+  kv: EditorPluginKv
+): Promise<ProjectAsset[] | MetaFields> {
+  const requestProperties = await getPropertiesFromRequest(request);
+  const url = new URL(request.url);
+  const urlSearchParams = new URLSearchParams(url.search);
+  const search = urlSearchParams.get("search");
+
+  const settingType = getPath(request.url);
+
+  // Handle settings and meta fields
+  if (settingType === "meta_fields") {
+    return {};
+  } else if (settingType) {
+    return {};
+  }
+
+
+  const headers = {
+    headers: {
+      "Accept-Version": "v1",
+      "Authorization": `Client-ID ${env.API_KEY}`,
+    },
+  };
+  const url2 = `${API}/search/photos?query=${search}&content_filter=high&per_page=20`;
+  console.log('headers', headers);
+  console.log('url2', url2);
+  console.log('env.API_KEY', env.API_KEY);
+  const response = await fetch(
+    url2,
+    headers
+  );
+
+  const json = (await response.json()).results as Photo[];
+  console.log("json", json);
+  return mapAssetToVevAsset(json);
+}
+
+registerVevPlugin({
+  id: "unsplashassetsource",
+  name: "Unsplash",
+  type: EditorPluginType.ASSET_SOURCE,
+  icon: "https://logowik.com/content/uploads/images/unsplash8609.jpg",
+  form: [
+    {
+      type: "string",
+      name: "API_KEY",
+      title: "API Key",
+    },
+  ],
+  handler: handler,
+});
+
+export default handler;
